f1b10d7d032a5bb27ba940d2dfad2afae4391a87
Jaroslav Šmolík <grissius@gmail.com>
2019-01-07 09:44:48 +0100

    Refactor def controller and export contoller interface

--- src/lib/service/middleware.ts
+++ src/lib/service/middleware.ts
@@ -1,24 +1,6 @@
-import { RequestHandler, Router } from 'express';
-import { CreateHandler, DeleteHandler, DetailHandler, HandlerCreators, ListHandler, UpdateHandler } from './handlers';
-
-interface CrudController<T, C> {
-    detailAction: (handler: DetailHandler<T, C>, idName: string) => RequestHandler;
-    createAction: (handler: CreateHandler<T, C>) => RequestHandler;
-    updateAction: (handler: UpdateHandler<T, C>, idName: string) => RequestHandler;
-    deleteAction: (handler: DeleteHandler<T, C>, idName: string) => RequestHandler;
-    listAction: (handler: ListHandler<T, C>) => RequestHandler;
-}
-
-const getDefaultController = <T, C extends {}>(): CrudController<T, C> => ({
-    detailAction: (handler, idName) => ({ params }, res) =>
-        handler(params[idName], {} as C).then(result => res.json(result)),
-    createAction: handler => ({ body }, res) => handler(body, {} as C).then(result => res.json(result)),
-    updateAction: (handler, idName) => ({ params, body }, res) =>
-        handler(params[idName], body, {} as C).then(result => res.json(result)),
-    deleteAction: (handler, idName) => ({ params }, res) =>
-        handler(params[idName], {} as C).then(result => res.json(result)),
-    listAction: handler => ({ params }, res) => handler(params, {} as C).then(result => res.json(result)),
-});
+import { Router } from 'express';
+import { getDefaultController } from './controller';
+import { HandlerCreators } from './handlers';
 
 export const createMiddlewareFactory = <T, C>(handlers: HandlerCreators<T, C>) => {
     const createMiddleware = (resourceName: string, idName: string = 'resourceId') => {
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ export {
     CrudContext,
     ListContext,
 } from './lib/context/crudContext';
+export { CrudController } from './lib/service/controller';
 export { Operation } from './lib/context/operation';
 export { Definitions } from './lib/settings/definitions';
 export { CrudRepository } from './lib/settings/repository';

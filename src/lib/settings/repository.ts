8e7d0e9a6358c08b1b73d25c91e7e478282c44c1
Michal Vlasák <michal.vlasak@ackee.cz>
2019-02-11 10:35:21 +0100

    Fix deleteById type

--- src/lib/settings/repository.ts
+++ src/lib/settings/repository.ts
@@ -2,6 +2,6 @@ export interface CrudRepository<T> {
     detailById: (id: number, options: any) => PromiseLike<T>;
     create: (data: any, options: any) => PromiseLike<T>;
     updateById: (id: number, data: any, options: any) => PromiseLike<T>;
-    deleteById: (id: number, options: any) => PromiseLike<T>;
+    deleteById: (id: number, options: any) => PromiseLike<any>;
     list: (filters: any, options: any) => PromiseLike<T[]>;
 }
